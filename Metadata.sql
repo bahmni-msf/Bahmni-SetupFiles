33133ba8e5945059e7b7318fd5f17b4971ec7560
sreekalyanthoughtworks <51774069+sreekalyanthoughtworks@users.noreply.github.com>
2020-05-11 14:31:50 +0530

    Removing the role permission part

--- Metadata.sql
+++ Metadata.sql
@@ -52,19 +52,3 @@ INSERT INTO role (role, description, uuid) values ('bahmni-document-uploader', '
 INSERT INTO role (role, description, uuid) values ('Doctor', 'Role for the doctor', uuid());
 INSERT INTO role (role, description, uuid) values ('Nurse', 'Role for the nurse', uuid());
 INSERT INTO role (role, description, uuid) values ('RegistrationClerk', 'RegistrationClerk', uuid());
-
-
-
--- Give Required Privileges to superman
-set @superman_id = null;
-select  user_id into @superman_id from openmrs.users where username = 'superman';
-INSERT INTO openmrs.user_role (user_id, role) VALUES (@superman_id, 'Privilege Level: Full');
-INSERT INTO openmrs.user_role (user_id, role) VALUES (@superman_id, 'bahmni-document-uploader');
-INSERT INTO openmrs.user_role (user_id, role) VALUES (@superman_id, 'Doctor');
-INSERT INTO openmrs.user_role (user_id, role) VALUES (@superman_id, 'Nurse');
-INSERT INTO openmrs.user_role (user_id, role) VALUES (@superman_id, 'RegistrationClerk');
-INSERT INTO openmrs.user_role (user_id, role) VALUES (@superman_id, 'System Developer');
-INSERT INTO openmrs.user_role (user_id, role) VALUES (@superman_id, 'Provider');
-
--- Give 'Get Locations' Privilege to Anonymous
-INSERT INTO openmrs.role_privilege (role, privilege) VALUES ('Anonymous', 'Get Locations');
